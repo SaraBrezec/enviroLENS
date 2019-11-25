2a371dad1eee59ff59a0746fb1fa7969c608ccc2
Sara Brezec <sarab@atena.ijs.si>
2019-11-25 12:19:41 +0100

    update config

--- microservice-template/microservice/config/config.py
+++ microservice-template/microservice/config/config.py
@@ -19,11 +19,7 @@ class Config(object):
 class ProductionConfig(Config):
     """Production configuration"""
 
-<<<<<<< HEAD
-    # TODO: add required secret configurations
-    ENV='production'
-    SECRET_KEY=os.getenv('PROD_SECRET_KEY')
-=======
+
     # done
     ENV='production'
     SECRET_KEY=os.getenv('PROD_SECRET_KEY')
@@ -32,18 +28,10 @@ class ProductionConfig(Config):
         'password': os.getenv('PROD_PG_PASSWORD')
     }
 
->>>>>>> 3b9e35fa6378757dfa66f7074e3fb9ce2688b1c6
-
 
 class DevelopmentConfig(Config):
     """Development configuration"""
 
-<<<<<<< HEAD
-    # TODO: add required secret configurations
-    ENV='development'
-    DEBUG = True
-    SECRET_KEY=os.getenv('DEV_SECRET_KEY')
-=======
     # done
     ENV='development'
     DEBUG = True
@@ -53,18 +41,10 @@ class DevelopmentConfig(Config):
         'password': os.getenv('DEV_PG_PASSWORD')
     }
 
->>>>>>> 3b9e35fa6378757dfa66f7074e3fb9ce2688b1c6
-
 
 class TestingConfig(Config):
     """Testing configuration"""
 
-<<<<<<< HEAD
-    # TODO: add required secret configurations
-    ENV='testing'
-    TESTING = True
-    SECRET_KEY=os.getenv('TEST_SECRET_KEY')
-=======
     # done
     ENV='testing'
     TESTING = True
@@ -72,5 +52,3 @@ class TestingConfig(Config):
     DATABASE={
         'database': os.getenv('TEST_PG_DATABASE'),
         'password': os.getenv('TEST_PG_PASSWORD')
-    }
->>>>>>> 3b9e35fa6378757dfa66f7074e3fb9ce2688b1c6
